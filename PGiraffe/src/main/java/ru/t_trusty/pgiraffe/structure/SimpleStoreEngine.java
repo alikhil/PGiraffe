8829b4be80484206c1e47f958fd5cac346fc4882
Alik Khilazhev <alikhil@mail.ru>
2019-03-04 11:44:41 +0300

    Add some progress

--- PGiraffe/src/main/java/ru/t_trusty/pgiraffe/structure/SimpleStoreEngine.java
+++ PGiraffe/src/main/java/ru/t_trusty/pgiraffe/structure/SimpleStoreEngine.java
@@ -15,6 +15,7 @@ public class SimpleStoreEngine implements  StoreEngine {
     private Map<Object, PGiraffeEdge> edges = new ConcurrentHashMap<>();
     private Map<Object, PGiraffeVertex> vertices = new ConcurrentHashMap<>();
     private Map<Element, Map<String, Object>> properties = new ConcurrentHashMap<>();
+    private Map<Vertex, Set<Edge>> vertexEdges = new ConcurrentHashMap<>();
 
     public SimpleStoreEngine(EntityManagerFactory entityManagerFactory) {
 
@@ -24,6 +25,12 @@ public class SimpleStoreEngine implements  StoreEngine {
     @Override
     public void saveEdge(PGiraffeEdge edge) {
         edges.put(edge.id(), edge);
+
+        vertexEdges.putIfAbsent(edge.inVertex(), new HashSet<>());
+        vertexEdges.putIfAbsent(edge.outVertex(), new HashSet<>());
+
+        vertexEdges.get(edge.inVertex()).add(edge);
+        vertexEdges.get(edge.outVertex()).add(edge);
     }
 
     @Override
@@ -33,16 +40,33 @@ public class SimpleStoreEngine implements  StoreEngine {
         // but what is vertex?
         // we can ensure every prop it has
         // or we can ensure only label
+
+        // p.s.
+        // all props are ensured in time of their creation
     }
 
     @Override
-    public void removeVertex(PGiraffeVertex pGiraffeVertex) {
+    public void removeVertex(PGiraffeVertex vertex) {
+        if (properties.containsKey(vertex)) {
+            properties.remove(vertex);
+        }
         // also should remove all properties of vertex
     }
 
     @Override
-    public Iterator<Edge> getEdges(PGiraffeVertex pGiraffeVertex, Direction direction, String... edgeLabels) {
-        return null;
+    public Iterator<Edge> getEdges(PGiraffeVertex vertex, Direction direction, String... edgeLabels) {
+        return vertexEdges.getOrDefault(vertex, new HashSet<>())
+                .stream()
+                .filter(edge -> matchesDirection(vertex, edge, direction))
+                .filter(edge -> Arrays.stream(edgeLabels).anyMatch(edge.label()::equals))
+                .iterator();
+    }
+
+    private boolean matchesDirection(PGiraffeVertex vertex, Edge edge, Direction direction) {
+        // todo: ensure that it's true
+        return direction == Direction.BOTH
+                || (edge.inVertex().id() == vertex.id() && direction == Direction.IN)
+                || (edge.outVertex().id() == vertex.id() && direction == Direction.OUT);
     }
 
     @Override
@@ -78,9 +102,9 @@ public class SimpleStoreEngine implements  StoreEngine {
 
     @Override
     public <V> void removeProperty(PGiraffeProperty property) {
-        // seems like we don't need such method
         if (properties.containsKey(property.element())) {
             Map<String, Object> mp = properties.get(property.element());
+            // todo: remove checks, they are redundant
             if (mp.containsKey(property.key())) {
                 mp.remove(property.key());
             }
@@ -89,6 +113,8 @@ public class SimpleStoreEngine implements  StoreEngine {
 
     @Override
     public void removeEdge(PGiraffeEdge edge) {
-
+        // todo: finish writing this method
+        vertexEdges.remove(edge.inVertex());
+        vertexEdges.remove(edge.outVertex());
     }
 }
